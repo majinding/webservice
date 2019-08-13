a5750d33e4eb579ffb40bbe309cbbbd2ca9a97d0
Jingjing.Ma <majinding888@foxmail.com>
2019-08-13 21:25:05 +0800

    增加自定义输入和输出对象节点，及自定义operationName

--- server/cxf-springboot2/src/main/java/cn/majingjing/majjwscxf/ws/Hello.java
+++ server/cxf-springboot2/src/main/java/cn/majingjing/majjwscxf/ws/Hello.java
@@ -4,6 +4,9 @@ import javax.jws.WebMethod;
 import javax.jws.WebParam;
 import javax.jws.WebResult;
 import javax.jws.WebService;
+import javax.xml.ws.Holder;
+import javax.xml.ws.RequestWrapper;
+import javax.xml.ws.ResponseWrapper;
 
 /**
  * @Author JingjingMa
@@ -17,4 +20,19 @@ public interface Hello {
 
     @WebMethod()
     Dto namedto(Dto dto);
+
+    @WebMethod(operationName = "xxxx")
+    @RequestWrapper(localName = "myRequest")
+    @ResponseWrapper(localName = "myResp")
+    void inputAndOutPut(
+            @WebParam(name = "name")
+                    String name,
+            @WebParam(name = "id")
+                    String id,
+            @WebParam(name = "dto", mode = WebParam.Mode.OUT)
+                    Holder<Dto> dto,
+            @WebParam(name = "bizId",mode = WebParam.Mode.OUT)
+                    Holder<Integer> bizId,
+            @WebParam(name = "bizName", mode = WebParam.Mode.OUT)
+                    Holder<String> bizName);
 }
--- server/cxf-springboot2/src/main/java/cn/majingjing/majjwscxf/ws/HelloPortImpl.java
+++ server/cxf-springboot2/src/main/java/cn/majingjing/majjwscxf/ws/HelloPortImpl.java
@@ -1,6 +1,11 @@
 package cn.majingjing.majjwscxf.ws;
 
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
 import javax.jws.WebService;
+import javax.xml.ws.Holder;
+import javax.xml.ws.RequestWrapper;
+import javax.xml.ws.ResponseWrapper;
 
 /**
  * @Author JingjingMa
@@ -17,8 +22,21 @@ public class HelloPortImpl implements Hello {
     @Override
     public Dto namedto(Dto dto) {
         Dto d = new Dto();
-        d.setId("server :"+dto.getId());
-        d.setName("server :"+dto.getName());
+        d.setId("server :" + dto.getId());
+        d.setName("server :" + dto.getName());
         return d;
     }
+
+    @Override
+    public void inputAndOutPut(String name, String id, Holder<Dto> dto, Holder<Integer> bizId, Holder<String> bizName) {
+        Dto d = new Dto();
+        d.setId("server :1");
+        d.setName("server : majingjing");
+        dto.value = d;
+        bizId.value = 1;
+        bizName.value = "biz-" + name;
+    }
+
+
+
 }
